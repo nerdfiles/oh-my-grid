a798d9f3ba9555213b759e4442258518bbf05a59
nerdfiles <nerdfiles@gmail.com>
2021-02-16 16:11:06 -0600

    Add jsdoc to container and server modules

---
+++ src/container/index.js
@@ -0,0 +1,39 @@
+/**
+ * @module container
+ * @description
+ */
+const { createContainer, asValue, asFunction } = require('awilix');
+
+const app = require('../app');
+const server = require('../inter/http/server');
+const router = require('../inter/http/router');
+const auth = require('../inter/http/authn');
+const config = require('../../config');
+const logger = require('../infra/logging/logger');
+const database = require('../infra/database');
+const jwt = require('../infra/jwt');
+const response = require('../infra/support/response');
+const date = require('../infra/support/date');
+const repository = require('../infra/repos');
+
+
+const container = createContainer();
+
+container
+  .register({
+    app: asFunction(app).singleton(),
+    server: asFunction(server).singleton(),
+    router: asFunction(router).singleton(),
+    logger: asFunction(logger).singleton(),
+    db: asFunction(db).singleton(),
+    auth: asFunction(auth).singleton(),
+    jwt: asFunction(jwt).singleton(),
+    response: asFunction(response).singleton(),
+    date: asFunction(date).singleton(),
+    config: asValue(config),
+    repository: asFunction(repository).singleton()
+  });
+
+module.exports = container;
+
+// EOF
--- src/inter/http/server.js
+++ src/inter/http/server.js
@@ -1,6 +1,32 @@
-const express = require('express')
+/**
+ * @module interfaces/http/server
+ * @description
+ */
+const express = require('express');
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const casl = require('casl')
 const seneca = require('seneca')
 const clusterluck = require('clusterluck')
+
+
+module.exports = ({ config, router, logger, auth }) => {
+  const app = express();
+
+  app.disable('x-powered-by');
+  app.use(auth.initialize());
+  app.use(router);
+
+  app.use(express.static('public'));
+
+  return {
+    app,
+    start: () => new Promise((resolve) => {
+      const http = app.listen(config.PORT, () => {
+        const { port } = http.address();
+        logger.info(`🎉 API - Open http://localhost:${port}`);
+      });
+    })
+  };
+};
+
