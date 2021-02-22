4bc445f6d2ee7f6352ea69eec9255449278b5ab8
nerdfiles <nerdfiles@gmail.com>
2021-02-22 14:27:47 -0600

    Add stub method

--- src/inter/http/service/api-service.js
+++ src/inter/http/service/api-service.js
@@ -2,11 +2,25 @@
  * @name api-service
  * @module interfaces/http/service
  */
+const got = require('got');
+const cache = {};
 
 module.exports = ({ config }) => {
   const API_KEY = config.API_KEY;
+  const url = 'http://api.eia.gov/series/?series_id=sssssss&api_key=' + API_KEY + '[&num=10][&out=json]';
+
+  const reply = () => {
+    return new Promise(async (resolve, reject) => {
+      if (cache.hasOwnProperty(url)) {
+        return resolve(cache[url]);
+      }
+      cache[url] = await got(url);
+      resolve(cache[url].body);
+    });
+  };
   return {
-    API_KEY
+    API_KEY,
+    reply: reply
   };
 };
 
