bf69ba9ede1b2c36b570e3986e5b8f2710441274
nerdfiles <nerdfiles@gmail.com>
2021-02-22 22:18:28 -0600

    Repair token

--- src/domain/token/index.js
+++ src/domain/token/index.js
@@ -3,4 +3,16 @@
  * @module domain 
  */
 
+const t = require('tcomb');
+
+
+const Token = t.struct({
+  email: t.String,
+  username: t.maybe(t.String),
+  password: t.String
+});
+
+
+module.exports = Token;
+
 // EOF
